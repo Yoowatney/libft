9dbca1ad7a381c5bc0105ac3399b2f388d234720
yoowatney <mcwatney@naver.com>
2021-05-07 19:12:05 +0900

    1

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,5 +1,17 @@
-/*#include <stdio.h>*/
-/*#include <string.h>*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoyoo <yoyoo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/07 19:09:15 by yoyoo             #+#    #+#             */
+/*   Updated: 2021/05/07 19:09:16 by yoyoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *source, size_t num)
@@ -10,7 +22,7 @@ void	*ft_memcpy(void *dest, const void *source, size_t num)
 
 	while (i < num)
 	{
-		*(char *)dest = *(char *)source;
+		*(unsigned char *)dest = *(unsigned char *)source;
 		i++;
 		dest++;
 		/** printf("dest : %p\n", dest); */
@@ -19,19 +31,26 @@ void	*ft_memcpy(void *dest, const void *source, size_t num)
 	return (dest);
 }
 
-/*int main()*/
-/*{*/
-	/*int ori[25];*/
-	/*for (int i = 0; i < 20; i++)*/
-	/*{*/
-		/*ori[i] = i + 1;*/
-	/*}*/
-	/*ft_memcpy(ori + 4, ori + 2, sizeof(int) * 17);*/
-	/*for (int i = 0; i < 20; i++)*/
-	/*{*/
-		/*printf("%d ", ori[i]);*/
-	/*}*/
-/*}*/
+int main()
+{
+	char *ptr1;
+	char *ptr2;
+
+	/** memcpy(ptr1, ptr2, 2); */
+	printf("%p\n", ptr1);
+	return (0);
+	printf("%p\n", memcpy(ptr1, ptr2, 2));
+	int ori[25];
+	for (int i = 0; i < 20; i++)
+	{
+		ori[i] = i + 1;
+	}
+	ft_memcpy(ori + 4, ori + 2, sizeof(int) * 17);
+	for (int i = 0; i < 20; i++)
+	{
+		printf("%d ", ori[i]);
+	}
+}
 
 /** int main() */
 /** { */
