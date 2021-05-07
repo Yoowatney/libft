3caa95e0d567e16629ddad8a0097e77eeb529a44
yoowatney <mcwatney@naver.com>
2021-05-08 02:35:37 +0900

    1

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -5,21 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoyoo <yoyoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
-/*   Created: 2021/05/07 19:09:15 by yoyoo             #+#    #+#             */
-/*   Updated: 2021/05/07 19:09:16 by yoyoo            ###   ########.fr       */
+/*   Created: 2021/05/07 19:13:37 by yoyoo             #+#    #+#             */
+/*   Updated: 2021/05/08 00:50:09 by yoyoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
-=======
-/*   Created: 2021/05/06 13:42:17 by yoyoo             #+#    #+#             */
-/*   Updated: 2021/05/06 15:13:55 by yoyoo            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
->>>>>>> 148165ef79daa98e72d222e6ba740ba20a100e86
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *source, size_t num)
@@ -27,40 +19,36 @@ void	*ft_memcpy(void *dest, const void *source, size_t num)
 	unsigned long i;
 
 	i = 0;
+	if (!dest && !source)
+		return (dest);
 	while (i < num)
 	{
-<<<<<<< HEAD
-		*(unsigned char *)dest = *(unsigned char *)source;
-=======
 		*((unsigned char *)dest + i) = *(unsigned char *)source;
->>>>>>> 148165ef79daa98e72d222e6ba740ba20a100e86
 		i++;
 		source++;
 	}
 	return (dest);
 }
-<<<<<<< HEAD
 
-int main()
-{
-	char *ptr1;
-	char *ptr2;
-
-	/** memcpy(ptr1, ptr2, 2); */
-	printf("%p\n", ptr1);
-	return (0);
-	printf("%p\n", memcpy(ptr1, ptr2, 2));
-	int ori[25];
-	for (int i = 0; i < 20; i++)
-	{
-		ori[i] = i + 1;
-	}
-	ft_memcpy(ori + 4, ori + 2, sizeof(int) * 17);
-	for (int i = 0; i < 20; i++)
-	{
-		printf("%d ", ori[i]);
-	}
-}
+/** int main() */
+/** { */
+/**     char *ptr1 = NULL; */
+/**     char *ptr2 = NULL; */
+/**     [> printf("%p\n", memcpy(NULL, NULL, 2)); <] */
+/**     [> return (0); <] */
+/**     printf("%p\n", ft_memcpy(ptr1, ptr2, 2)); */
+/**     return (0); */
+/**     int ori[25]; */
+/**     for (int i = 0; i < 20; i++) */
+/**     { */
+/**         ori[i] = i + 1; */
+/**     } */
+/**     ft_memcpy(ori + 4, ori + 2, sizeof(int) * 17); */
+/**     for (int i = 0; i < 20; i++) */
+/**     { */
+/**         printf("%d ", ori[i]); */
+/**     } */
+/** } */
 
 /** int main() */
 /** { */
@@ -72,5 +60,3 @@ int main()
 /**         printf("%d ", dest[i]); */
 /**     } */
 /** } */
-=======
->>>>>>> 148165ef79daa98e72d222e6ba740ba20a100e86
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yoyoo <yoyoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 18:45:34 by yoyoo             #+#    #+#             */
-/*   Updated: 2021/05/06 20:50:17 by yoyoo            ###   ########.fr       */
+/*   Updated: 2021/05/08 02:03:18 by yoyoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@
 #include <stddef.h>
 
 void	ft_bzero(void *ptr, size_t size);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 void	*ft_memccpy(void *dest, const void *source, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-int	ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
+int		ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
 void	*ft_memcpy(void *dest, const void *source, size_t num);
 void	*ft_memmove(void *dest, const void *src, size_t num);
 void	*ft_memset(void *ptr, int value, size_t num);
